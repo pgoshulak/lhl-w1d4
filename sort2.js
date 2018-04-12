925d6d51dcedd016ea74fe77c40c115a4cb52358
Peter Goshulak <pgoshulak@gmail.com>
2018-04-12 11:57:14 -0400

    Complete advanced sorting exercise

--- sort2.js
+++ sort2.js
@@ -4,3 +4,19 @@ var students = [
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
+
+var result = students.sort(function(studentA, studentB) {
+  // If names are the same, sort older student first
+  if (studentA.name === studentB.name) {
+    return studentB.age - studentA.age
+  } 
+  
+  // Sort by name
+  if (studentA.name > studentB.name) {
+    return 1
+  } else {
+    return -1
+  }
+})
+
+console.log(result)
